ab9f2089eda2a9685c68a50e3b3b35d641b1b0f9
dragoncoder5 <jivamarco@gmail.com>
2024-10-29 11:07:30 +0100

    Model Category, Order and Product done

---
+++ ThaiBubbles-H6/Model/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ThaiBubbles_h6.Model
+{
+
+      public class Product
+      {
+        [Key]
+        public int ProductID { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public int price { get; set; } = 0;
+
+        public string Description { get; set; } = string.Empty;
+        public bool Favorite { get; set; } = false;
+
+        public int? CategoryId { get; set; }
+        public Category? category { get; set; }
+
+
+
+        [JsonIgnore]
+        public List<ProductList?> orderlists { get; set; } = new List<ProductList?>();
+      }
+}
+
+      
+
