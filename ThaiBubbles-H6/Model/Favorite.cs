d0af2f8b5038a8aa506bc0dc02a1aad95604c531
Haittham Muhammed Abu Zahra <hait0029@elev.tec.dk>
2024-10-29 10:51:53 +0100

    models city, favorite and user done

---
+++ ThaiBubbles-H6/Model/Favorite.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThaiBubbles_H6.Model
+{
+    public class Favorite
+    {
+        [Key]
+        public int FavoriteID { get; set; } = 0;
+        public int? UserId { get; set; }
+
+
+        public User? UserFK { get; set; }
+    }
+}
