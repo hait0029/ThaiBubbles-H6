dc08d6a1fe7a1499a4050fbdc0492c603acf0284
Haittham Muhammed Abu Zahra <hait0029@elev.tec.dk>
2024-10-31 11:15:39 +0100

    dataseed

--- ThaiBubbles-H6/Model/Product.cs
+++ ThaiBubbles-H6/Model/Product.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-using ThaiBubbles_H6.Model;
-
-namespace ThaiBubbles_h6.Model
+﻿namespace ThaiBubbles_h6.Model
 {
 
-      public class Product
+    public class Product
       {
         [Key]
         public int ProductID { get; set; }
         public string Name { get; set; } = string.Empty;
 
-        public int price { get; set; } = 0;
+        public int Price { get; set; } = 0;
 
         public string Description { get; set; } = string.Empty;
         //public bool Favorite { get; set; } = false;
