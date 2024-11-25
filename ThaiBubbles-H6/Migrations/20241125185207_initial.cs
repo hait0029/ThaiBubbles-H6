e9e63eedb4dae41c7176f05dcbba08a34979c54a
dragoncoder5 <jivamarco@gmail.com>
2024-11-25 19:52:51 +0100

    sync

--- ThaiBubbles-H6/Migrations/20241125185207_initial.cs
+++ ThaiBubbles-H6/Migrations/20241125185207_initial.cs
@@ -60,6 +60,7 @@ namespace ThaiBubbles_H6.Migrations
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Price = table.Column<int>(type: "int", nullable: false),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     CategoryId = table.Column<int>(type: "int", nullable: true)
                 },
                 constraints: table =>
